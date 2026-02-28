06410bc9a7caee69a99328b1778b3eb0cb7712c9
Heinrich37 <belforthenrich2004@gmail.com>
2026-02-28 09:04:21 -0400

    primeiro commit

---
+++ newfile.py
@@ -0,0 +1,7 @@
+
+print("Ola mundo")
+
+i = 0
+while i < 10:
+	print(f'Bom dia meu povo, hoje  gittttt {1}')
+	i += 1
---
+++ newfile2.py
@@ -0,0 +1,5 @@
+x = input(int("Escolha um numero: ")) 
+y = input(int("Escolha o segundo numero: "))
+res = y*x
+print(res)
+
